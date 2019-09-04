10f02d6dc8d646405ae58609e83cfe5a194bb936
cshorvath <horvathcsa@gmail.com>
2019-09-04 14:06:36 +0000

    ui fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import AddMovie from "./components/AddMovie";
 import MovieList from "./components/MovieList";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 
 function App() {
     return (
         <React.Fragment>
+	    <Row className="mb-3">
                 <Col>
                     <AddMovie/>
                 </Col>
+	    </Row>
+	    <Row>
                 <Col>
                     <MovieList/>
                 </Col>
+	    </Row>
         </React.Fragment>
     );
 }
